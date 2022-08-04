f8db94191e488f18d8d56ea8501e4dc71d25200e
Salnitta <tatyana.saln@mail.ru>
2022-08-04 11:52:28 +0300

    add string sorting

--- Lesson04/Program.cs
+++ Lesson04/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿/* СОРТИРОВКА ВЫБОРОМ
 [6, 15, 2, 9, -3]
 MIN = 6
 6 < 15
@@ -23,27 +23,53 @@ MIN = 15
 15 > 9
 [-3, 2, 6, 9, 15]
 */
-Console.WriteLine("Введите кол-во элементов массива:");
-int n = Convert.ToInt32(Console.ReadLine());
-// Заполнение массива
-int[] array = new int[n];
-for (int i = 0; i < n; i++)
+
+// Сортировка выбором чисел
+// Console.WriteLine("Введите кол-во элементов массива:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// // Заполнение массива
+// int[] array = new int[n];
+// for (int i = 0; i < n; i++)
+// {
+//     Console.Write("Введите число: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine();
+// Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+// // Cортировка
+// for (int i = 0; i < n - 1; i++)
+// {
+//     int MinIndex = i;
+//     for (int j = i + 1; j < n; j++)
+//     {
+//         if (array[j] < array[MinIndex])
+//             MinIndex = j; 
+//     }
+//     int temp;
+//     temp = array[MinIndex];
+//     array[MinIndex] = array[i];
+//     array[i] = temp;
+// }
+// Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+
+
+// Сортировка выбором строк
+string[] array = new string[5];
+for (int i = 0; i < 5; i++)
 {
-    Console.Write("Введите число: ");
-    array[i] = Convert.ToInt32(Console.ReadLine());
+    array[i] = Console.ReadLine();
 }
+Console.Write("Начальный массив: [" + string.Join(", ", array) + "]");
 Console.WriteLine();
-Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
-// Cортировка
-for (int i = 0; i < n - 1; i++)
+for (int i = 0; i < 4; i++)
 {
     int MinIndex = i;
-    for (int j = i + 1; j < n; j++)
+    for (int j = i + 1; j < 5; j++)
     {
-        if (array[j] < array[MinIndex])
+        if (array[j].Length < array[MinIndex].Length)
             MinIndex = j; 
     }
-    int temp;
+    string temp;
     temp = array[MinIndex];
     array[MinIndex] = array[i];
     array[i] = temp;
